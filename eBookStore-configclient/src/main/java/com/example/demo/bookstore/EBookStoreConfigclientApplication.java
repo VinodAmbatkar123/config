5ea53df8331ef72050292c4a965d301241ff217b
VinodAmbatkar123 <100355487+VinodAmbatkar123@users.noreply.github.com>
2022-10-15 22:52:44 +0530

    Add files via upload

---
+++ eBookStore-configclient/src/main/java/com/example/demo/bookstore/EBookStoreConfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookStoreConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookStoreConfigclientApplication.class, args);
+	}
+
+}
